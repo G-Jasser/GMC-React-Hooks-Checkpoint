902d81feb5320efd120b5cd7ba3ea2e28c574ddf
Jasser Gharb <123377848+G-Jasser@users.noreply.github.com>
2023-08-07 19:25:10 +0100

    .

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { Form, InputGroup } from 'react-bootstrap'
 
-const Filter = ({title, rating}) => {
-  
-    return (
-    <div>Filter</div>
+const Filter = ({ setSearchByTitle, setSearchByRating }) => {
+
+  const handleSearchByTitleChange = (element) => setSearchByTitle(element.target.value)
+  const handleSearchByRatingChange = (element) => setSearchByRating(element.target.value)
+
+
+  return (
+    <div className='movie-adder'>
+      <h3 style={{ marginBottom: 20 }}>Movie Filter:</h3>
+      <InputGroup className="mb-3">
+        <InputGroup.Text>
+          Title:
+        </InputGroup.Text>
+        <Form.Control placeholder="Movie Title" onChange={handleSearchByTitleChange}/>
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <InputGroup.Text>Rating:</InputGroup.Text>
+        <Form.Control placeholder="x out of 10" onChange={handleSearchByRatingChange}/>
+        <InputGroup.Text>/10</InputGroup.Text>
+      </InputGroup>
+    </div>
   )
-}
-Filter.defaultProps = {
-    title:"",
-    rating: 0
 }
 
 export default Filter
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,49 +2,61 @@ import React, { useState } from 'react'
 import movies from '../assets/movies'
 import MovieCard from './MovieCard'
 import { Button, Form, InputGroup } from 'react-bootstrap'
+import Filter from './Filter'
 
 
 const MovieList = () => {
   const [moviesList, setMoviesList] = useState(movies)
-  let [title, setTitle] = useState("")
+
+  const [title, setTitle] = useState("")
   const handleTitleChange = (e) => setTitle(e.target.value)
-  let [rating, setRating] = useState(0.0)
+
+  const [rating, setRating] = useState(0.0)
   const handleRatingChange = (e) => setRating(e.target.value)
-  let [description, setDescription] = useState("")
+
+  const [description, setDescription] = useState("")
   const handleDescriptionChange = (e) => setDescription(e.target.value)
-  let [posterURL, setPosterURL] = useState("")
+
+  const [posterURL, setPosterURL] = useState("")
   const handlePosterURLChange = (e) => setPosterURL(e.target.value)
-  
-  const handleAddMovie = () => setMoviesList([{title,rating,description,posterURL}, ...moviesList])
-  const handleClear = () => { setTitle(""); setRating(0,0); setDescription(""); setPosterURL("") }
+
+  // const [movieDetails, setMovieDetails] = useState({title:"",rating:0,description:"",posterURL:""})
+
+  const [searchByTitle, setSearchByTitle] = useState("")
+  const [searchByRating, setSearchByRating] = useState(0)
+
+  const handleAddMovie = () => setMoviesList([{ title, rating, description, posterURL }, ...moviesList])
+  const handleClear = () => { setTitle(""); setRating(0, 0); setDescription(""); setPosterURL("") }
+
+
 
   return (
     <>
       <div className='movie-adder'>
-        <h3 style={{marginBottom:20}}>Movie Adder:</h3>
+        <h3 style={{ marginBottom: 20 }}>Movie Adder:</h3>
         <InputGroup className="mb-3">
           <InputGroup.Text>
             Title:
           </InputGroup.Text>
-          <Form.Control placeholder="Movie Title" onChange={handleTitleChange}/>
+          <Form.Control placeholder="Movie Title" onChange={handleTitleChange} />
         </InputGroup>
 
         <InputGroup className="mb-3">
           <InputGroup.Text>Rating:</InputGroup.Text>
-          <Form.Control placeholder="x out of 10" onChange={handleRatingChange}/>
+          <Form.Control placeholder="x out of 10" onChange={handleRatingChange} />
           <InputGroup.Text>/10</InputGroup.Text>
         </InputGroup>
 
         <InputGroup className='mb-3'>
           <InputGroup.Text>Description:</InputGroup.Text>
-          <Form.Control as="textarea" placeholder="Movie Description" onChange={handleDescriptionChange}/>
+          <Form.Control as="textarea" placeholder="Movie Description" onChange={handleDescriptionChange} />
         </InputGroup>
 
         <InputGroup className="mb-3">
           <InputGroup.Text id="basic-addon3">
             Poster URL:
           </InputGroup.Text>
-          <Form.Control id="basic-url" aria-describedby="basic-addon3" placeholder='https://example.com/poster.png' onChange={handlePosterURLChange}/>
+          <Form.Control id="basic-url" aria-describedby="basic-addon3" placeholder='https://example.com/poster.png' onChange={handlePosterURLChange} />
         </InputGroup>
 
         <div className='movie-buttons'>
@@ -52,12 +64,18 @@ const MovieList = () => {
           <Button variant="outline-secondary" onClick={handleClear}>Clear</Button>
         </div>
       </div>
+
+      <Filter setSearchByTitle={setSearchByTitle} setSearchByRating={setSearchByRating} />
+
       <div className='movies-section'>
         <h3>Movies Section:</h3>
         <div className='movies-container'>
-
           {
-            moviesList.map(
+            moviesList.filter(
+              (movie) => (movie.title.toLowerCase().includes(searchByTitle.toLowerCase())
+                || movie.description.toLowerCase().includes(searchByTitle.toLowerCase()))
+                && movie.rating >= searchByRating
+            ).map(
               (movie) => <MovieCard movieDetails={movie} key={movie.title} />
             )
           }
