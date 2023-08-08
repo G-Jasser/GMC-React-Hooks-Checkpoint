89dbdb4eba7fbca5355f309531a3bb82fc45ce9b
Jasser Gharb <123377848+G-Jasser@users.noreply.github.com>
2023-08-08 18:50:38 +0100

    really cool

--- src/components/MovieTrailer.jsx
+++ src/components/MovieTrailer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useOutletContext, useParams } from 'react-router-dom'
+import Xsvg from "../assets/x-symbol.svg"
+import { Link, useOutletContext, useParams } from 'react-router-dom'
 
 const MovieTrailer = () => {
   const movieID = useParams()
@@ -14,17 +15,20 @@ const MovieTrailer = () => {
     
       ( movieDetails ?
         ( 
-        <div className='movie-adder'>
-          <div className='col-6'>
+        <div className='movie-trailer'>
+          <Link to="/">
+            <img src={Xsvg} alt="X" />
+          </Link>
+          <div className='trailer_container'>
             <h1>{movieDetails.title}</h1>
-            <br />
-            <h3>Trailer:</h3>
-            <iframe width="560" height="315" 
-            src={movieDetails.trailer}
-            title="YouTube video player" frameBorder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            allowFullScreen></iframe>
-            <br />
+            <div className='col-6'>
+              <h3>Trailer:</h3>
+              <iframe width="560" height="315" 
+              src={movieDetails.trailer}
+              title="YouTube video player" frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowFullScreen></iframe>
+            </div>
             <p>{movieDetails.description}</p>
 
           </div>
